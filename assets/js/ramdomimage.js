87bc214bf2ace5e691fb6708a5da227725ecfe49
jaffamonkey <paullittlebury@gmail.com>
2025-02-22 20:45:07 +0000

    updates

--- assets/js/ramdomimage.js
+++ assets/js/ramdomimage.js
@@ -1,19 +1,23 @@
-function randomImage() {
-  var pics = ["https://terrormar.club/images/knife.png"
-  ];
+(function ($) {
+  'use strict';
 
-  var img = '';
-  var showHide = Math.random() * 2;
+  function randomImage() {
+    var pics = ["https://terrormar.club/images/knife.png"
+    ];
 
-  if (showHide = 1) {
-    for (ii = 0; ii < pics.length; ii++) {
-      positiontop = Math.random() * 500;
-      positionright = Math.random() * 800;
-      zindex = ii;
+    var img = '';
+    var showHide = Math.random() * 2;
 
-      img += '<img src="' + pics[ii] + '" style="top: ' + positiontop + 'px;' + 'right: ' + positionright + 'px;' + 'z-index: ' + zindex + ';">';
+    if (showHide = 1) {
+      for (ii = 0; ii < pics.length; ii++) {
+        positiontop = Math.random() * 500;
+        positionright = Math.random() * 800;
+        zindex = ii;
+
+        img += '<img src="' + pics[ii] + '" style="top: ' + positiontop + 'px;' + 'right: ' + positionright + 'px;' + 'z-index: ' + zindex + ';">';
+      }
+
+      document.getElementById('area').innerHTML = img;
     }
-
-    document.getElementById('area').innerHTML = img;
   }
-}
+})
