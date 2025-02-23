44bc14c1b3ff1ff6f237288cbcc669e2238b8786
jaffamonkey <paullittlebury@gmail.com>
2025-02-23 19:26:27 +0000

    updates

--- exampleSite/static/js/randomimage.js
+++ exampleSite/static/js/randomimage.js
@@ -1,22 +1,22 @@
 function randomImage() {
     var fileNames = [
-        "https://terrormar.club/images/knife.png",
-        "https://terrormar.club/images/blank.png",
-        "https://terrormar.club/images/knife.png"
+      "https://terrormar.club/images/knife.png",
+      "https://terrormar.club/images/blank.png",
+      "https://terrormar.club/images/knife.png"
     ];
     var randomIndex = Math.floor(Math.random() * fileNames.length);
     // document.getElementById("background").style.background = 'url(' + fileNames[randomIndex] + ')';
-    document.getElementById("background").src = "' + fileNames[randomIndex] + '";
-}
-
-function getRandomTime() {
-    return 1000;
-    // return Math.round(Math.random() * 5000);
-}
-
-(function loop() {
-    setTimeout(function () {
+    document.getElementById("background").src='' + fileNames[randomIndex] + '';
+  }
+  
+  function getRandomTime() {
+          return 1000;
+      // return Math.round(Math.random() * 5000);
+  }
+  
+  (function loop() {
+      setTimeout(function() {
         randomImage();
-        loop();
+      loop();
     }, getRandomTime())
-})();
+  })();
