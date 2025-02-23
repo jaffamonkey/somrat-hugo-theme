d2ffb26fe88f0a90e99159a590fd5187d77265db
jaffamonkey <paullittlebury@gmail.com>
2025-02-23 13:26:15 +0000

    updates

---
+++ static/plugins/ramdomimage.js
@@ -0,0 +1,27 @@
+function randomImage() {
+  var fileNames = [
+    "https://terrormar.club/images/knife.png",
+    "https://terrormar.club/images/knife.png",
+    "https://terrormar.club/images/knife.png",
+    "https://terrormar.club/images/knife.png",
+    "https://terrormar.club/images/knife.png",
+    // "https://terrormar.club/images/blank.png",
+    // "https://terrormar.club/images/blank.png",
+    // "https://terrormar.club/images/blank.png",
+    // "https://terrormar.club/images/blank.png",
+    // "https://terrormar.club/images/blank.png",
+    "https://terrormar.club/images/blank.png"];
+  var randomIndex = Math.floor(Math.random() * fileNames.length);
+  document.getElementById("background").style.background = 'url(' + fileNames[randomIndex] + ')';
+}
+function doSomething() { }
+(function loop() {
+  var rand = Math.round(Math.random() * 9999999999);
+  setTimeout(function () {
+    doSomething();
+    loop();
+  }, rand);
+}());
+
+randomImage();
+setInterval(randomImage, doSomething());
