2074aa0757b75eb95af94a8addd276db9335ae1a
jaffamonkey <paullittlebury@gmail.com>
2025-02-24 03:14:01 +0000

    updates

--- exampleSite/static/js/randomimage.js
+++ exampleSite/static/js/randomimage.js
@@ -11,13 +11,13 @@ function randomImage() {
         "https://terrormar.club/images/blank.png",
     ];
     var randomIndex = Math.floor(Math.random() * fileNames.length);
-    var randomLeftPosition = Math.floor(Math.random() * 300) + 50;
-    var randomTopPosition = Math.floor(Math.random() * 380) + 100;
-    var elem = document.getElementById("background");
+    // var randomLeftPosition = Math.floor(Math.random() * 300) + 50;
+    // var randomTopPosition = Math.floor(Math.random() * 380) + 100;
+    // var elem = document.getElementById("background");
     // document.getElementById("background").style.background = 'url(' + fileNames[randomIndex] + ')';
-    // document.getElementById("background").src='' + fileNames[randomIndex] + '';
+    document.getElementById("background").src='' + fileNames[randomIndex] + '';
     // setAttributes(elem, { "src": "" + fileNames[randomIndex] + "", "left": "" + randomLeftPosition + "", "top": "" + randomTopPosition + "" });
-    setAttributes(elem, { "src": "" + fileNames[randomIndex] + "" });
+    // setAttributes(elem, { "src": "" + fileNames[randomIndex] + "" });
 }
 
 function getRandomTime() {
