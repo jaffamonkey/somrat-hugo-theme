e4b01d2533bd31b971f5681c34a78bc5423e07a5
jaffamonkey <paullittlebury@gmail.com>
2025-02-24 02:44:04 +0000

    updates

--- exampleSite/static/js/randomimage.js
+++ exampleSite/static/js/randomimage.js
@@ -16,7 +16,7 @@ function randomImage() {
     var elem = document.getElementById("background");
     // document.getElementById("background").style.background = 'url(' + fileNames[randomIndex] + ')';
     // document.getElementById("background").src='' + fileNames[randomIndex] + '';
-    setAttributes(elem, { "src": "" + fileNames[randomIndex] + "", "left": "" + randomLeftPosition + "", "top": "" + randomTopPosition + "" });
+    setAttributes(elem, { "background-image": "" + fileNames[randomIndex] + "", "left": "" + randomLeftPosition + "", "top": "" + randomTopPosition + "" });
 }
 
 function getRandomTime() {
