52898ff8418120d199164338d160661f01f4333a
jaffamonkey <paullittlebury@gmail.com>
2025-02-24 00:15:47 +0000

    updates

--- exampleSite/static/js/randomimage.js
+++ exampleSite/static/js/randomimage.js
@@ -1,23 +1,27 @@
 function randomImage() {
     var fileNames = [
-      "https://terrormar.club/images/blank.png",
-      "https://terrormar.club/images/blank.png",
-      "https://terrormar.club/images/fish.gif",
-      "https://terrormar.club/images/blank.png",
-      "https://terrormar.club/images/blank.png"
+        "https://terrormar.club/images/blank.png",
+        "https://terrormar.club/images/blank.png",
+        "https://terrormar.club/images/fish.gif",
+        "https://terrormar.club/images/blank.png",
+        "https://terrormar.club/images/blank.png"
     ];
     var randomIndex = Math.floor(Math.random() * fileNames.length);
     // document.getElementById("background").style.background = 'url(' + fileNames[randomIndex] + ')';
-    document.getElementById("background").src='' + fileNames[randomIndex] + '';
-  }
-  
-  function getRandomTime() {
-      return Math.floor(Math.random() * 3000) + 1000
-  }
-  
-  (function loop() {
-      setTimeout(function() {
+    document.getElementById("background").src = '' + fileNames[randomIndex] + '';
+}
+
+function getRandomTime() {
+    return Math.floor(Math.random() * 3000) + 1000
+}
+
+function showHide() {
+    return Math.floor(Math.random() * 4) + 1
+}
+
+if (function loop() {
+    setTimeout(function () {
         randomImage();
-      loop();
+        loop();
     }, getRandomTime())
-  })();
+});
