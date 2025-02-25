44e2b694419bae017535ffbcd750b5dad158d343
jaffamonkey <paullittlebury@gmail.com>
2025-02-25 04:23:03 +0000

    updates

--- exampleSite/static/js/randomimage.js
+++ exampleSite/static/js/randomimage.js
@@ -13,12 +13,12 @@ function randomImage() {
         "https://terrormar.club/images/blank.png",
     ];
     var randomIndex = Math.floor(Math.random() * fileNames.length);
-    var randomLeftPosition = Math.floor(Math.random() * 300) + 50;
-    var randomTopPosition = Math.floor(Math.random() * 380) + 100;
-    var elem = document.getElementById("background");
+    // var randomLeftPosition = Math.floor(Math.random() * 300) + 50;
+    // var randomTopPosition = Math.floor(Math.random() * 380) + 100;
+    // var elem = document.getElementById("background");
     // document.getElementById("background").style.background = 'url(' + fileNames[randomIndex] + ')';
-    // document.getElementById("background").src='' + fileNames[randomIndex] + '';
-    setAttributes(elem, { "src": "" + fileNames[randomIndex] + "", "padding-left": "" + randomLeftPosition + "px", "padding-top": "" + randomTopPosition + "px" });
+    document.getElementById("background").src='' + fileNames[randomIndex] + '';
+    // setAttributes(elem, { "src": "" + fileNames[randomIndex] + "", "left": "" + randomLeftPosition + "", "top": "" + randomTopPosition + "" });
     // setAttributes(elem, { "src": "" + fileNames[randomIndex] + "", "object-position:": "top " + randomTopPosition + " left " + randomLeftPosition + "" });
     // setAttributes(elem, { "src": "" + fileNames[randomIndex] + "" });
 }
