80dc04006b0f9618e646d61cf9f3c366f8d4cb9a
jaffamonkey <paullittlebury@gmail.com>
2025-02-25 07:41:29 +0000

    updates

--- exampleSite/static/js/randomimage.js
+++ exampleSite/static/js/randomimage.js
@@ -14,7 +14,7 @@ function randomImage() {
     ];
     var randomIndex = Math.floor(Math.random() * fileNames.length);
     var randomLeftPosition = Math.floor(Math.random() * 300) + 50;
-    var randomTopPosition = Math.floor(Math.random() * 350) + 100;
+    var randomTopPosition = Math.floor(Math.random() * 400) + 100;
     var elem = document.getElementById("background");
     // document.getElementById("background").style.background = 'url(' + fileNames[randomIndex] + ')';
     // document.getElementById("background").src='' + fileNames[randomIndex] + '';
